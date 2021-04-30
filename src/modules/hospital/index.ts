e917bfbd3adf6b388de0f986f7af3b444034d378
Daniel Pham <nguyen.pc@live.com>
2021-04-30 23:21:11 +0700

    feat: add security

--- src/modules/hospital/index.ts
+++ src/modules/hospital/index.ts
@@ -3,11 +3,11 @@ import express from 'express';
 import hospitalActions from './hospital.controller';
 
 const hospitalRoutes = (app: express.Application): void => {
-  app.post('/api/v1/hospital', hospitalActions.createHospitalAction);
+  app.post('/api/v1/hospital', middleware.authenticate, hospitalActions.createHospitalAction);
   app.get('/api/v1/hospital', hospitalActions.fetchHospitalAction);
   app.get('/api/v1/hospital/:hospitalId', hospitalActions.fetchHospitalInfoAction);
-  app.put('/api/v1/hospital/:hospitalId', hospitalActions.updateHospitalInfoAction);
-  app.delete('/api/v1/hospital/:hospitalId', hospitalActions.deleteHospitalAction);
+  app.put('/api/v1/hospital/:hospitalId', middleware.authenticate, hospitalActions.updateHospitalInfoAction);
+  app.delete('/api/v1/hospital/:hospitalId', middleware.authenticate, hospitalActions.deleteHospitalAction);
 };
 
 export default hospitalRoutes;
--- src/modules/services/index.ts
+++ src/modules/services/index.ts
@@ -3,9 +3,9 @@ import express from 'express';
 import serviceActions from './service.controller';
 
 const serviceRoutes = (app: express.Application): void => {
-  app.post('/api/v1/service', serviceActions.createServiceAction);
+  app.post('/api/v1/service', middleware.authenticate, serviceActions.createServiceAction);
   app.get('/api/v1/service', serviceActions.fetchServiceAction);
-  app.put('/api/v1/service/:id', serviceActions.updateServiceAction);
+  app.put('/api/v1/service/:id', middleware.authenticate, serviceActions.updateServiceAction);
   app.get('/api/v1/service/:id', serviceActions.getServiceByIdOrSlugAction);
 };
 
