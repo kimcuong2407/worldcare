b9cf9ced3835578389c46793282fc1603c750eaa
Daniel Pham <nguyen.pc@live.com>
2021-09-01 10:54:27 +0700

    fix: validate order

--- src/modules/orders/actions/process.handler.ts
+++ src/modules/orders/actions/process.handler.ts
@@ -5,6 +5,7 @@ import OrderAbstractHandler from './order-handler.abstract';
 import { ORDER_ACTIONS, ORDER_STATUS } from '../constant';
 import OrderCollection from '../order.collection';
 import { ValidationFailedError } from '@app/core/types/ErrorTypes';
+import OrderItemCollection from '../order-item.collection';
 
 class ProcessOrderHandler extends OrderAbstractHandler {
   // eslint-disable-next-line class-methods-use-this
@@ -30,6 +31,10 @@ class ProcessOrderHandler extends OrderAbstractHandler {
   }
 
   async validate (order: any, payload: any) {
+    const lineItemsCount = await OrderItemCollection.count({orderNumber: get(order, 'orderNumber'),});
+    if(lineItemsCount <= 0) {
+      throw new ValidationFailedError('Vui lòng nhập vào sản phẩm trong đơn hàng trước.')
+    }
     return Promise.resolve(true);
   }
 }
