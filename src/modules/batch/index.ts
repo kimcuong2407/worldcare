096af3ee8bfd0fae84d5c87deac0c66ee5ee1b7e
tuannguyen <nvntuan93@gmail.com>
2021-12-18 03:04:15 +0700

    fix(batch): remove debug

--- src/modules/batch/index.ts
+++ src/modules/batch/index.ts
@@ -5,10 +5,9 @@ import {CLINIC_RESOURCES, CORE_ACTIONS} from '@core/permissions';
 
 const batchRoutes = (app: express.Application): void => {
 
-  app.post('/api/v1/batch',
-  // middleware.authorization([
-  //   [CLINIC_RESOURCES.sale, CORE_ACTIONS.write]
-  // ]),
+  app.post('/api/v1/batch', middleware.authorization([
+    [CLINIC_RESOURCES.sale, CORE_ACTIONS.write]
+  ]),
   batchActions.createBatchAction);
 
   app.get('/api/v1/batch', middleware.authorization([
